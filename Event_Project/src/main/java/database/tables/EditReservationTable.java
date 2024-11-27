1a6bbe383103054f3e0f3d54c694d2f1115fadee
Nikolaos Lazaridis <nikoslazaridis10@gmail.com>
2024-11-27 14:27:04 +0200

    Reservation table

--- Event_Project/src/main/java/database/tables/EditReservationTable.java
+++ Event_Project/src/main/java/database/tables/EditReservationTable.java
@@ -4,10 +4,120 @@
  */
 package database.tables;
 
+import com.google.gson.Gson;
+import database.DB_Connection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mainClasses.Reservation;
+
 /**
  *
  * @author nikos
  */
 public class EditReservationTable {
+
+    public void addReservationFromJSON(String json) throws ClassNotFoundException {
+        Reservation r = jsonToReservation(json);
+        createNewReservation(r);
+    }
+
+    public Reservation databaseToReservation(int id) throws SQLException, ClassNotFoundException {
+        Connection con = DB_Connection.getConnection();
+        Statement stmt = con.createStatement();
+
+        ResultSet rs;
+        try {
+            rs = stmt.executeQuery("SELECT * FROM reservations WHERE reservation_id= '" + id + "'");
+            rs.next();
+            String json = DB_Connection.getResultsToJSON(rs);
+            Gson gson = new Gson();
+            Reservation bt = gson.fromJson(json, Reservation.class);
+            return bt;
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Reservation jsonToReservation(String json) {
+        Gson gson = new Gson();
+        Reservation r = gson.fromJson(json, Reservation.class);
+        return r;
+    }
+
+    public String ReservationToJSON(Reservation r) {
+        Gson gson = new Gson();
+
+        String json = gson.toJson(r, Reservation.class);
+        return json;
+    }
+
+    public void updateReservation(int reservationID, String status) throws SQLException, ClassNotFoundException {
+        Connection con = DB_Connection.getConnection();
+        Statement stmt = con.createStatement();
+        String updateQuery = "UPDATE reservations SET status='" + status + "' WHERE reservation_id= '" + reservationID + "'";
+        stmt.executeUpdate(updateQuery);
+        stmt.close();
+        con.close();
+    }
+
+    public void createReservationTable() throws SQLException, ClassNotFoundException {
+        Connection con = DB_Connection.getConnection();
+        Statement stmt = con.createStatement();
+        String sql = "CREATE TABLE reservation ("
+                + "reservation_id INTEGER NOT NULL AUTO_INCREMENT, "
+                + "reservation_tickets INTEGER NOT NULL, "
+                + "reservation_date DATE NOT NULL, "
+                + "reservation_payment_amount INTEGER NOT NULL, "
+                + "reservation_status ENUM('ACTIVE', 'CANCELED', 'COMPLETE') NOT NULL, "
+                + "client_username VARCHAR(50) NOT NULL, "
+                + "event_id INTEGER NOT NULL, "
+                + "ticket_id INTEGER NOT NULL, "
+                + "FOREIGN KEY (client_username) REFERENCES clients(client_username), "
+                + "FOREIGN KEY (event_id) REFERENCES events(event_id), "
+                + "FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id), "
+                + "PRIMARY KEY (reservation_id)"
+                + ")";
+        stmt.execute(sql);
+        stmt.close();
+        con.close();
+    }
+
+    /**
+     * Establish a database connection and add in the database.
+     *
+     * @throws ClassNotFoundException
+     */
+    public void createNewReservation(Reservation res) throws ClassNotFoundException {
+        try {
+            Connection con = DB_Connection.getConnection();
+
+            Statement stmt = con.createStatement();
+
+            String insertQuery = "INSERT INTO bookings "
+                    + "(reservation_id, reservation_tickets, reservation_date, reservation_payment_amount, reservation_status) "
+                    + "VALUES ("
+                    + "'" + res.getReservationID() + "', "
+                    + "'" + res.getReservationTickets() + "', "
+                    + "'" + res.getReservationDate() + "', "
+                    + "'" + res.getReservationPaymentAmount() + "', "
+                    + "'" + res.getReservationStatus() + "')";
+            //stmt.execute(table);
+
+            stmt.executeUpdate(insertQuery);
+            System.out.println("# The reservation was successfully added in the database.");
+
+            /* Get the member id from the database and set it to the member */
+            stmt.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EditReservationTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
--- Event_Project/src/main/java/mainClasses/Reservation.java
+++ Event_Project/src/main/java/mainClasses/Reservation.java
@@ -4,10 +4,59 @@
  */
 package mainClasses;
 
+enum Status {
+    ACTIVE, CANCELED, COMPLETE
+};
+
 /**
  *
  * @author nikos
  */
 public class Reservation {
 
+    private int reservationID;
+    private int reservationTickets;
+    private int reservationPaymentAmount;
+    private String reservationDate;
+    private Status reservationStatus;
+
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public int getReservationTickets() {
+        return reservationTickets;
+    }
+
+    public void setReservationTickets(int reservationTickets) {
+        this.reservationTickets = reservationTickets;
+    }
+
+    public int getReservationPaymentAmount() {
+        return reservationPaymentAmount;
+    }
+
+    public void setReservationPaymentAmount(int reservationPaymentAmount) {
+        this.reservationPaymentAmount = reservationPaymentAmount;
+    }
+
+    public String getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(String reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public Status getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(Status reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
 }
